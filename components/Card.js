a4bd74cc67e5092bd339c2096e41d0b6367e6773
DeveshRamiya <68691515+DeveshRamiya@users.noreply.github.com>
2023-03-03 13:16:17 -0500

    Front end changes
    Made it look presentable

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styles from "./Card.module.css";
 
 export default function Card(props) {
   const {
@@ -16,31 +15,29 @@ export default function Card(props) {
   }
 
   return (
-    <div>
-      <div className={styles.card}>
-        <div className={styles.card__header}>
-          <h2 className={styles.card__title}>{title}</h2>
-          <button className={styles.card__button} onClick={handlePairSelected}>
+    <div className="bg-blue p-4">
+      <div className="bg-white rounded-lg border border-gray-200 shadow-lg overflow-hidden hover:bg-blue-50 transition-colors duration-300">
+        <div className="px-6 py-4">
+          <h2 className="text-lg font-medium text-gray-800">{title}</h2>
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-sm text-gray-600 font-medium">Address:</span>
+            <span className="text-sm text-gray-800 break-all">{address}</span>
+          </div>
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-sm text-gray-600 font-medium">{tradingVolume24h}:</span>
+            <span className="text-sm text-gray-800">{tradingVolume24h}</span>
+          </div>
+          <div className="flex justify-between items-center mb-2">
+            <span className="text-sm text-gray-600 font-medium">APY:</span>
+            <span className="text-sm text-gray-800">{APY}</span>
+          </div>
+          <div className="flex justify-between items-center">
+            <span className="text-sm text-gray-600 font-medium">{impermanentLoss}:</span>
+            <span className="text-sm text-gray-800">{impermanentLoss}</span>
+          </div>
+          <button className="bg-indigo-500 text-white rounded-lg px-4 py-2 mt-4 hover:bg-indigo-600" onClick={handlePairSelected}>
             Select
           </button>
-        </div>
-        <div className={styles.card__body}>
-          <div className={styles.card__row}>
-            <span className={styles.card__label}>Address:</span>
-            <span className={styles.card__value}>{address}</span>
-          </div>
-          <div className={styles.card__row}>
-            <span className={styles.card__label}>{tradingVolume24h}:</span>
-            <span className={styles.card__value}>{tradingVolume24h}</span>
-          </div>
-          <div className={styles.card__row}>
-            <span className={styles.card__label}>APY:</span>
-            <span className={styles.card__value}>{APY}</span>
-          </div>
-          <div className={styles.card__row}>
-            <span className={styles.card__label}>{impermanentLoss}:</span>
-            <span className={styles.card__value}>{impermanentLoss}</span>
-          </div>
         </div>
       </div>
     </div>
--- components/Rectangle.js
+++ components/Rectangle.js
@@ -2,8 +2,13 @@ import styles from "./Rectangle.module.css";
 
 export default function Rectangle(props) {
   return (
-    <div className={styles.box}>
-      <div className={styles.box}>{props.children}</div>
+    <div className="relative flex items-center justify-center w-screen h-screen overflow-hidden">
+      <div className="absolute inset-0 z-0 bg-gray-900"></div>
+      <div className="absolute inset-0 z-0 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900"></div>
+      <div className="absolute inset-0 bg-blur" style={{ backgroundImage: `url(${props.bgImage})` }}></div>
+      <div className="z-10 animate-pulse">
+        {props.children}
+      </div>
     </div>
   );
 }
